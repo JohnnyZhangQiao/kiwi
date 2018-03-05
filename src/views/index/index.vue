5430476ff57d10d051bed64fd3f0b9c8f5067b86
Johnny Zhang <285552401@qq.com>
2018-03-05 21:58:35 +0800

    update index

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,22 @@
 <template>
-	<div class="index-body">
-		
-	</div>
+  <div class="index-body">
+    <div class="index-body-main">
+      <p class="index-body-title">kiwi.book</p>
+      <div class="index-body-tab">
+        <p :class="{'tab-active':searchType == 'keywords'}" @click="searchType='keywords'">keywords</p>
+        <p :class="{'tab-active':searchType == 'title'}" @click="searchType='title'">title</p>
+        <p :class="{'tab-active':searchType == 'other'}" @click="searchType='other'">other</p>
+      </div>
+      <div class="index-body-tab-body" v-show="searchType == 'keywords'">
+        <div class="input-group kiwi-input-group">
+          <input type="text" class="form-control search-input-style" placeholder="name" aria-label="name">
+          <div class="input-group-append kiwi-text-icon">
+            <span class="input-group-text"><i class="fa fa-search"></i></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,21 +27,64 @@ export default {
   },
   data() {
   	return {
-
+      searchType: 'keywords',
   	}
   },
   mounted() {
-  	console.log(typeof(bootstrap))
   },
 }
 </script>
 
-<style lang="less" scoped="">
+<style lang="less" scoped>
+.index-body {
+  width: 100%;
+  text-align: center;
+}
+.index-body-main {
+  margin: 120px auto 0 auto;
+  width: 80%;
+  max-width: 720px;
+  .index-body-title {
+    font-size: 10rem;
+    color: #799ee2;
+  }
+  .index-body-tab {
+    display: flex;
+    width: 100%;
+    p {
+      margin-right: 20px;
+      font-size: 1.8rem;
+      &:hover {
+        color: #799ee2;
+        cursor: pointer;
+      }
+    }
+    .tab-active {
+      border-bottom: 2px solid #799ee2;
+      color: #799ee2;
+    }
+  }
+  .kiwi-input-group {
+    margin: 0 auto;
+    width: 100%;
+    .search-input-style {
+      height: 40px;
+    }
+    .kiwi-text-icon:hover {
+      cursor: pointer;
+      opacity: .8;
+    }
+  }
+}
+
 /*移动端设备*/
 @media (max-width: 766px) {
-	.test {
-		display: none;
-	}
+	.index-body-main {
+    margin-top: 60px;
+    .index-body-title {
+      font-size: 4.6rem;
+    }
+  }
 }
 
 
